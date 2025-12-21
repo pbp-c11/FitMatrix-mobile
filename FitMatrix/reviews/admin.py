bcd1ab9bd8bf3d246cbcdc7cdb6b90bf999ba339
Ega Sudjali <egasudjali1989@gmail.com>
2025-12-21 14:21:59 +0700

    done"

--- FitMatrix/reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("user", "place", "rating", "created_at")
-    list_filter = ("rating", "created_at")
-    search_fields = ("user__username", "place__name", "body")
--- fitmatrix_flutter/lib/data/providers/collections_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../models/wishlist_collection.dart';
-import '../api_service.dart';
-
-final collectionsProvider =
-    FutureProvider<List<WishlistCollection>>((ref) {
-  return ref.read(apiServiceProvider).fetchCollections();
-});
